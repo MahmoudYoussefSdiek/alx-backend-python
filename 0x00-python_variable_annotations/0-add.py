1d33902916ca0d4016635df6ec70ffa389c306bc
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-11 20:49:53 +0000

    Create 0-add.py file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides an add function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes two float arguments and returns their sum as a float.
+    """
+    return a + b
