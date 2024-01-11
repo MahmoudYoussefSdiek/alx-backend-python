9f5c108a37fd03396f160af466b53c0ac2b673a0
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-11 21:05:07 +0000

    Create 2-floor.py file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a concat function.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float argument and returns
+    the floor of the float.
+    """
+    return math.floor(n)
