c0a45ed87c3e77dc84d3cceb7550045dc1515a71
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-11 20:53:49 +0000

    Create 1-concat.py file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a concat function.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function takes two string arguments and returns their
+    concatenation as a string.
+    """
+    return str1 + str2
