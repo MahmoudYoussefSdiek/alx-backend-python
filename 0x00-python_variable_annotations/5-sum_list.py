dc59c088edc0da12882bfe3886ddc38efd580767
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-11 22:31:14 +0000

    Create 5-sum_list.py file

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a sum_list function.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculates the sum of a list of floating-point numbers.
+    """
+    return sum(input_list)
