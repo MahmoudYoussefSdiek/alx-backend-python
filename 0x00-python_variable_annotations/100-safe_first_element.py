4e4eee83d26377c318618e0480bc6b537ce15061
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-12 00:51:23 +0000

    Create 100-safe_first_element.py file

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a safe_first_element function.
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Returns the first element of the sequence if it exists,
+    otherwise returns None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
